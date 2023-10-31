6d92bfc23e8908438ba243b5b9f174da2267fc15
Sunny Chung <sunnychung@live.hk>
2023-10-31 21:07:16 +0800

    update to minify the repo according to the compose multiplatform team's finding

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,15 +5,8 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.rememberScrollbarAdapter
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.LocalTextStyle
-import androidx.compose.material.Text
 import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontFamily
@@ -22,13 +15,8 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import kotlinx.coroutines.launch
-import org.jetbrains.compose.splitpane.ExperimentalSplitPaneApi
-import org.jetbrains.compose.splitpane.HorizontalSplitPane
-import org.jetbrains.compose.splitpane.rememberSplitPaneState
 
-@OptIn(ExperimentalSplitPaneApi::class)
 @Composable
 @Preview
 fun App() {
@@ -38,18 +26,17 @@ fun App() {
 
     LaunchedEffect(Unit) {
         coroutineScope.launch {
-//            val longText = (0 .. 300 * 1000).map { // around 300K data
-//                val c = "abcdefghijklmnABCDEFGHIJK123456\":{}[] \n".random().toString()
-//                if (c == "\n") {
-//                    "$c    "
-//                } else {
-//                    c
-//                }
-//            }
-//                .joinToString("")
+            val longText = (0 .. 300 * 1000).map { // around 300K data
+                val c = "abcdefghijklmnABCDEFGHIJK123456\":{}[] \n".random().toString()
+                if (it % 20 == 0) return@map "\r\n"
+                if (c == "\n") {
+                    "$c    "
+                } else {
+                    c
+                }
+            }
+                .joinToString("")
 
-            var longText = Thread.currentThread().contextClassLoader.getResourceAsStream("long-resp.txt")!!.readAllBytes().decodeToString()
-            longText = jacksonObjectMapper().readTree(longText).toPrettyString()
             println("longText has been prepared")
             text = text.copy(text = longText)
         }
@@ -70,40 +57,6 @@ fun App() {
             modifier = Modifier.align(Alignment.CenterEnd)
         )
     }
-
-//        HorizontalSplitPane(splitPaneState = rememberSplitPaneState(initialPositionPercentage = 0.1f)) {
-//            first(minSize = 5.dp) {
-//                Text(text = "abc")
-//            }
-//            second(minSize = 300.dp) {
-//                HorizontalSplitPane(splitPaneState = rememberSplitPaneState(initialPositionPercentage = 0.1f)) {
-//                    first(minSize = 10.dp) {
-//                        Text(text = "abc")
-//                    }
-//                    second(minSize = 200.dp) {
-//                        Column (modifier = Modifier.padding(horizontal = 8.dp)) {
-//                            Text(text = "def")
-//                            Box(modifier = Modifier.fillMaxWidth().weight(1f).padding(vertical = 8.dp)) {
-//                                TextField(
-//                                    value = text,
-//                                    onValueChange = { _ -> },
-//                                    textStyle = LocalTextStyle.current.copy(fontFamily = FontFamily.Monospace),
-//                                    readOnly = true,
-//                                    modifier = Modifier.align(Alignment.Center)
-//                                        .fillMaxSize()
-//                                        .verticalScroll(scrollState),
-//                                )
-//                                VerticalScrollbar(
-//                                    rememberScrollbarAdapter(scrollState),
-//                                    modifier = Modifier.align(Alignment.CenterEnd)
-//                                )
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
 }
 
 fun main() = application {
